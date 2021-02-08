e8aed934155a1a7af6d48b9ac2272005caa2fb5e
MisterAzix <flizi@live.fr>
2021-02-08 21:52:59 +0100

    Twitch stream start notification

--- index.js
+++ index.js
@@ -29,21 +29,62 @@ bot.once('ready', async () => {
 
 ############################################################`);
 
-await listener.listen();
-let prevStream = await apiClient.helix.streams.getStreamByUserId(userId);
+    await listener.listen();
+    let prevStream = await apiClient.helix.streams.getStreamByUserId(userId);
 
-const subscription = await listener.subscribeToStreamChanges(userId, async stream => {
-    console.log()
-    if (stream) {
-        if (!prevStream) {
-            console.log(`${stream.userDisplayName} just went live with title: ${stream.title}`);
+    const subscription = await listener.subscribeToStreamChanges(userId, async stream => {
+        if (stream) {
+            if (!prevStream) {
+                const user = await apiClient.helix.users.getUserById(userId);
+                const guild = bot.guilds.cache.get('544300325801164820');
+                const channel = guild.channels.cache.get(config.NOTIFICATION_CHANNEL_ID);
+
+                stream.getGame().then(g => {
+                    let embed = {
+                        "title": stream.title ?? 'Aucun titre trouvé',
+                        "url": `https://www.twitch.tv/${user.displayName}`,
+                        "color": 4721180,
+                        "fields": [
+                            {
+                                "name": "Jeu",
+                                "value": g.name ?? 'Non renseigné',
+                                "inline": true
+                            },
+                            {
+                                "name": "Viewers",
+                                "value": stream.viewers ?? 0,
+                                "inline": true
+                            }/* ,
+                        {
+                            "name": "Langue",
+                            "value": stream.language ?? 'FR',
+                            "inline": true
+                        } */
+                        ],
+                        "author": {
+                            "name": user.displayName,
+                            "url": `https://www.twitch.tv/${user.displayName}`,
+                            "icon_url": user.profilePictureUrl
+                        },
+                        "footer": {
+                            "text": `Rejoins moi maintenant : https://www.twitch.tv/${user.displayName}`
+                        },
+                        "image": {
+                            "url": `https://static-cdn.jtvnw.net/previews-ttv/live_user_${stream.userName}-960x540.jpg`
+                        }
+                    }
+
+                    channel.send(`Hey @everyone ! **${user.displayName}** est maintenant en direct sur https://www.twitch.tv/${user.displayName} ! Allez y jeter un oeil !`, { embed: embed });
+                });
+
+                //console.log(`${stream.userDisplayName} just went live with title: ${stream.title}`);
+            }
+        } else {
+            //const user = await apiClient.helix.users.getUserById(userId);
+            //console.log(`${user.displayName} just went offline`);
         }
-    } else {
-        const user = await apiClient.helix.users.getUserById(userId);
-        console.log(`${user.displayName} just went offline`);
-    }
-    prevStream = stream ?? null;
-});
+        prevStream = stream ?? null;
+    });
 
 });
 
