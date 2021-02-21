abe228690de7c2f99364c985efe337720471aefe
MisterAzix <flizi@live.fr>
2021-02-21 22:08:10 +0100

    Change listener hostname

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const apiClient = new ApiClient({ authProvider });
 bot.apiClient = apiClient;
 //const listener = new WebHookListener(apiClient, new NgrokAdapter(), { hookValidity: config.HOOK_VALIDITY });
 const listener = new WebHookListener(apiClient, new ReverseProxyAdapter({
-    hostName: 'maxencebreuilles.tech'
+    hostName: 'localhost'
 }));
 
 bot.commands = new Collection();
